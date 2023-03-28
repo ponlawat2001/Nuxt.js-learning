5ac565694827bc2c5c858f682527bbe7cdf7d4e5
mix23456 <mixsax2@gmail.com>
2023-03-28 16:14:34 +0700

    change carousel to splideslide

--- components/topupmenu.vue
+++ components/topupmenu.vue
@@ -1,27 +1,71 @@
 <template>
+    <span class="button-left">
+        <img :width="12" src="~/assets/images/icon/Icon_right-arrow.png" class="fa fa-angle-left" />
+    </span>
     <div class="topup-box">
-        <vueper-slides class="no-shadow" :slide-ratio="1 / 4" :visible-slides="6" slide-multiple :gap="5"
-            :dragging-distance="100" :autoplay="true" :fixed-height="true" :arrows="false"
-            :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }">
-            <vueper-slide class="topup-card" v-for="item in items" :key="i" :image="item.image" :title="item.title"
-                :content="item.content">
-            </vueper-slide>
-        </vueper-slides>
+        <Splide :options="settings">
+            <SplideSlide v-for="(item, i) in items" :key="i">
+                <img :src="item.image" :alt="item.title">
+                <div class="topuptext">{{ item.title }}</div>
+            </SplideSlide>
+        </Splide>
     </div>
 </template>
 
 <script>
-import { VueperSlides, VueperSlide } from 'vueperslides'
-import 'vueperslides/dist/vueperslides.css'
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/vue-splide/css';
+
 export default {
-
-    components: { VueperSlides, VueperSlide },
-
+    components: {
+        Splide,
+        SplideSlide,
+    },
     data: () => ({
+        settings: {
+            start: 0,
+            arrows: true,
+            type: 'loop',
+            perPage: 6,
+            rewind: true,
+            autoplay: true,
+            speed: '1000',
+            Width: '200px',
+            gap: '24px',
+            focus: 'left',
+            pagination: false,
+
+        },
+
         items: [
             {
                 title: 'IQIYI',
+                content: 'sdfdsf',
                 image: 'https://www.termtang.com/assets/images/termgame/IQIYI.png'
+            },
+            {
+                title: 'RoV Mobile MOBA',
+                image: 'https://www.termtang.com/assets/images/termgame/aov-2.png?v=3'
+            },
+            {
+                title: 'RoV Mobile MOBA',
+                image: 'https://www.termtang.com/assets/images/termgame/aov-2.png?v=3'
+            },
+            {
+                title: 'RoV Mobile MOBA',
+                image: 'https://www.termtang.com/assets/images/termgame/aov-2.png?v=3'
+            },
+            {
+                title: 'RoV Mobile MOBA',
+                image: 'https://www.termtang.com/assets/images/termgame/aov-2.png?v=3'
+            },
+            {
+                title: 'RoV Mobile MOBA',
+                image: 'https://www.termtang.com/assets/images/termgame/aov-2.png?v=3'
+            },
+            {
+                title: 'RoV Mobile MOBA',
+                image: 'https://www.termtang.com/assets/images/termgame/aov-2.png?v=3'
             },
             {
                 title: 'RoV Mobile MOBA',
@@ -50,31 +94,18 @@ export default {
 </script>
 
 <style>
+div.topup-box img {
+    width: 170px;
+}
+
+div.topup-box .topuptext {
+    color: #343a40;
+    font-size: 12px;
+    margin-top: 8px;
+}
+
 div.topup-box {
     margin: 8px 0 8px 0;
     width: 1200px;
-}
-
-div.topup-box .vueperslide__title {
-    font-size: 12px;
-    font-weight: 600;
-    color: gray;
-    position: absolute;
-    top: 0%;
-}
-
-div.topup-box .vueperslide__content {
-    font-size: 16px;
-    color: white;
-    position: absolute;
-}
-
-div.topup-box .vueperslides--fixed-height {
-    height: 190px;
-}
-
-div.topup-box .slidetext {
-    text-align: center;
-    color: black;
 }
 </style>
